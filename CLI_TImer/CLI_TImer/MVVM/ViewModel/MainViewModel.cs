14f75a4d15589ec5cbf0f202749fb88be2b53243
Majost <57063653+Maximilian-Strigl@users.noreply.github.com>
2023-01-20 19:42:26 +0100

    Added Breaktimer UI doesn't Update right now

--- CLI_TImer/CLI_TImer/MVVM/ViewModel/MainViewModel.cs
+++ CLI_TImer/CLI_TImer/MVVM/ViewModel/MainViewModel.cs
@@ -1,18 +1,19 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CLI_TImer.MVVM.Model;
+using CLI_TImer.Themes;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
-using System.Threading;
-using System.Windows.Navigation;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Runtime.CompilerServices;
-using CLI_TImer.MVVM.Model;
-using System.Windows.Media;
-using CLI_TImer.Themes;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace CLI_TImer.MVVM.ViewModel
 {
     public partial class MainViewModel : ObservableObject
     {
+        //Main Timer 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(MainTimerText))]
         public int mainSeconds = 1;
@@ -26,13 +27,35 @@ namespace CLI_TImer.MVVM.ViewModel
         [NotifyPropertyChangedFor(nameof(MainTimerText))]
         public int mainHours = 0;
 
+        public string MainTimerText => $"{MainHours}h {MainMinutes}m {MainSeconds}s";
+
+
+        //Break Timer
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(PauseTimerText))]
+        public int pauseSeconds = 1;
+
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(PauseTimerText))]
+        public int pauseMinutes = 10;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(PauseTimerText))]
+        public int pauseHours = 0;
+
+
+        public string PauseTimerText => $"{PauseHours}h {PauseMinutes}m {PauseSeconds}s";
+
         [ObservableProperty]
         public string enteredCommand;
 
         [ObservableProperty]
         public ObservableCollection<Command> commandHistory = new ObservableCollection<Command>();
 
-        public string MainTimerText => $"{MainHours}h {MainMinutes}m {MainSeconds}s";
+        
+
+        private bool isPaused = false;
 
         readonly Thread timerThread;
 
@@ -58,12 +81,16 @@ namespace CLI_TImer.MVVM.ViewModel
             switch (command) 
             {
                 case "work":
-                {
-                    work();
-                    break;
-                }
 
-            
+                    Work();
+                    break;
+                
+
+                case "break":
+                
+                    Pause();
+                    break;
+                
 
                 default:
                     CommandHistory.Add(new Command { title = "Error", answer = "unknown Command", output = "", gradientStops = Gradients.GradientStops()});
@@ -71,7 +98,7 @@ namespace CLI_TImer.MVVM.ViewModel
             }
         }
 
-        private void work()
+        private void Work()
         {
             MainMinutes = 45;
             MainSeconds = 1;
@@ -80,13 +107,31 @@ namespace CLI_TImer.MVVM.ViewModel
             CommandHistory.Add(work);
         }
 
+        private void Pause()
+        {
+            isPaused = true;
+            PauseMinutes = 20;
+            PauseSeconds = 1;
+
+            Command pause = new Command { title = "break", answer = "we are taking a break", output = PauseTimerText, gradientStops= Gradients.GradientStops()};
+            CommandHistory.Add(pause);
+        }
+
 
         //Timer Management
         private void Countdown()
         {
             while (true)
             {
-                if (MainSeconds > 0 && MainMinutes >= 0 && MainHours >= 0) MainTimer();
+                if (!isPaused)
+                {
+                    if (MainSeconds > 0 && MainMinutes >= 0 && MainHours >= 0) MainTimer();
+                }
+
+                if(isPaused) 
+                {
+                    PauseTimer();
+                }
 
                 Thread.Sleep(1000);
             }
@@ -110,5 +155,33 @@ namespace CLI_TImer.MVVM.ViewModel
                 MainHours--;
             }
         }
+
+        private void PauseTimer()
+        {
+            PauseSeconds--;
+
+            CommandHistory.Clear();
+
+            if(PauseHours == 0 && PauseMinutes == 0 && PauseSeconds == 0)
+            {
+                isPaused = false;
+                return;
+            }
+
+            if (PauseSeconds == 0)
+            {
+                PauseMinutes--;
+                PauseSeconds = 59;
+            }
+
+            if (PauseMinutes == 0 && PauseHours> 0)
+            {
+                PauseMinutes= 59;
+                PauseHours--;
+            }
+        }
     }
 }
+
+// Added Break
+//
