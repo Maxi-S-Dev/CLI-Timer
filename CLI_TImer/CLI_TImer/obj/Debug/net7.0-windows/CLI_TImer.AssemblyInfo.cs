8f0d4318e50e236581bb527574c47dc312deaf77
Majost <57063653+Maximilian-Strigl@users.noreply.github.com>
2023-01-20 23:35:50 +0100

    created Build version 0.1

--- CLI_TImer/CLI_TImer/obj/Debug/net7.0-windows/CLI_TImer.AssemblyInfo.cs
+++ CLI_TImer/CLI_TImer/obj/Debug/net7.0-windows/CLI_TImer.AssemblyInfo.cs
@@ -11,13 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CLI_TImer")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Maxi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLI_TImer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLI_TImer")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
--- CLI_TImer/CLI_TImer/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ CLI_TImer/CLI_TImer/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace CLI_TImer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CLI_TImer;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CLI_TImer;V0.1.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
